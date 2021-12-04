3a0b9019dc5de3a90bd398dd0ff10df4d9d7e4fa
Steven <stntsai93@gmail.com>
2021-12-03 19:40:33 -0500

    testing

---
+++ deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  // the below wallet is for testing only, no actual ether contained
+  'before mobile boat general hedgehog alter satoshi scrap other athlete salmon unique',
+  'https://rinkeby.infura.io/v3/cf0d54c2cc2e4978852c2c5e448bb19f'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
