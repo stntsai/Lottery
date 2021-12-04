7e653bcbabe0459f5bc389e3619e88f49e80fcf0
Steven <stntsai93@gmail.com>
2021-12-03 20:06:23 -0500

    more testing

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -69,6 +69,29 @@ describe('Lottery Contract', () => {
         } catch (err){
             assert(err);
         }
+    })
+
+    it('only manager can call pickWinner', async () => {
+        try{
+            await lottery.methods.pickWinner().send({
+                from: accounts[1],
+            })
+            assert(false);
+        } catch (err){
+            assert(err);
+        }
+    });
+
+    it('send money to the winner and resets the players array', async()=>{
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2','ether')
+        });
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+        await lottery.methods.pickWinner().send({from: accounts[0]});
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+        const difference =  finalBalance-initialBalance;
+        assert(difference > web3.utils.toWei('1.8','ether'));
 
     })
 })
